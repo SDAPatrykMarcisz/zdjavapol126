69c8611f5597f94d213e50cd0264f373b96fccf7
Patryk <patryk.marcisz.v2@gmail.com>
2023-03-12 09:39:47 +0100

    dodanie komentarza

--- src/main/java/pl/sda/entity/PersonEntity.java
+++ src/main/java/pl/sda/entity/PersonEntity.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class PersonEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //identity = przerzucenie odpowiedzialnosci za tworzenie ID na baze danych
     @Column(name = "ID")
     private Long id;
 
