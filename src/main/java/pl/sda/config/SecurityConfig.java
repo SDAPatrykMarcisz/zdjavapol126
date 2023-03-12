1a1d04550f9323c9716459ff448721c2bd26d87b
Patryk <patryk.marcisz.v2@gmail.com>
2023-03-12 10:50:55 +0100

    dodanie wstepnej konfiguracji security

--- src/main/java/pl/sda/config/SecurityConfig.java
+++ src/main/java/pl/sda/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package pl.sda.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -12,8 +13,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
+        http.csrf() //wylaczenie tokenow csrf
+                .disable()
+            .httpBasic() //wlaczenie logowania przez http basic.and()
+                .and()
+            .formLogin()
+                .disable()//wylaczenie formularza logowania
+            .authorizeRequests() //ustalenie ograniczen do poszczegolnych stron
+                .antMatchers(HttpMethod.GET,"/persons*/**").authenticated();
         return http.build();
     }
 
+    //spring.security.user.password=12345
 }
